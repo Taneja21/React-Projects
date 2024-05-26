cb8aeda28c3a06983964502ac259cd3ee1ebd1e0
Taneja21 <tusharaneja43@gmail.com>
2024-05-26 19:41:13 +0530

    Basic Setup without context api and btn functionality

--- Theme Changer/src/App.jsx
+++ Theme Changer/src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from "react";
+import Card from "./Components/Card";
+import ThemeBtn from "./Components/ThemeBtn";
 
 function App() {
   return (
     <>
-      <h1 className="bg-red-700 text-black text-4xl text-center py-12 px-12 mx-12">
-        Vite + Tailwind Setup
-      </h1>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="flex justify-center items-center h-screen bg-gray-100">
+            <Card
+              title="theme changer"
+              content="This is a simple card component created with React and styled using Tailwind CSS. Check the above Toggle Theme button to enable dark mode"
+              imageUrl="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDF8fGNhcmR8ZW58MHx8fHwxNjU1NjY2NjA0&ixlib=rb-1.2.1&q=80&w=400"
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
