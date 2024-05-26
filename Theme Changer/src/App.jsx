dccbede47fd9d73caca3dbcdaf9182b6acd29fbd
Taneja21 <tusharaneja43@gmail.com>
2024-05-26 19:17:02 +0530

    Proejct Setup

---
+++ Theme Changer/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-700 text-black text-4xl text-center py-12 px-12 mx-12">
+        Vite + Tailwind Setup
+      </h1>
+    </>
+  );
+}
+
+export default App;
