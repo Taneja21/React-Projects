8a2c5a4b6cc91950c66ab78e3ae0f2a5cdc45e4f
Taneja21 <tusharaneja43@gmail.com>
2024-05-27 22:30:01 +0530

    ToDo: Components In Progress

--- ToDo App/src/components/ToDoForm.jsx
+++ ToDo App/src/components/ToDoForm.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTodo } from "../contexts/index";
 
 function ToDoForm() {
+  const { addTodo } = useTodo();
+
+  const [todo, setTodo] = useState({});
+  function add(e) {
+    setTodo({ todo: e.target.value, completed: false });
+    addTodo(todo);
+  }
+
   return (
     <form className="flex">
       <input
         type="text"
         placeholder="Write Todo..."
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        value={todo}
+        onChange={add}
       />
       <button
         type="submit"
