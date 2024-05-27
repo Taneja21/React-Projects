9518b300d300039cae8226d5b1749c3da66ff1eb
Taneja21 <tusharaneja43@gmail.com>
2024-05-27 16:41:32 +0530

    Vite Setup with Tailwind

---
+++ ToDo App/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-700 text-black text-center text-5xl mx-12 py-5">
+        Vite Setup with Tailwind
+      </h1>
+    </>
+  );
+}
+
+export default App;
