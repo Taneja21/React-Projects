20994b273a0e2f75ba00776507c5151a011905cd
Taneja21 <tusharaneja43@gmail.com>
2024-06-04 21:18:47 +0530

    BlogPost: Appwrite Setup, Authentication setup

---
+++ BlogPost/src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appWriteUrl)
+      .setProject(config.appWriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        return this.login(email, password);
+      } else {
+        return userAccount;
+      }
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return this.account.createEmailPasswordSession(email, password);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return this.account.get();
+    } catch (err) {
+      throw err;
+    }
+
+    return null;
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
